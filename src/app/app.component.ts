cf870ed969919d949da507eeb67b7d85653cfd33
Raju Gandhi <raju.gandhi@gmail.com>
2017-02-20 15:11:27 -0500

    feat: trap Navigation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+  Resolve,
+  ResolveData
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  loading = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((e: Event) => {
+      this.navigationInterceptor(e);
+    });
+  }
+
+  navigationInterceptor(event: Event): void {
+    this.loading = this.isStart(event) ? true : false;
+  }
+
+  isStart(event: Event): boolean {
+    return event instanceof NavigationStart;
+  }
+
+  isEnd(event: Event): boolean {
+    return (event instanceof NavigationEnd)
+      || (event instanceof NavigationCancel)
+      || (event instanceof NavigationError);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, NavigationError } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
@@ -22,7 +22,9 @@ import { ClassesComponent } from './classes/classes.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {
+      enableTracing: true
+    }),
 
     MaterialModule.forRoot()
   ],
