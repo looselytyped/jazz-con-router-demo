e406e9a2edb8355a57be6711aa4ad4ea35f2e6b7
Raju Gandhi <raju.gandhi@gmail.com>
2017-03-03 15:11:13 -0500

    feat: add static data to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,24 @@ import { ClassesComponent } from './classes/classes.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
+import {
+  ClassesResolver
+} from './shared'
+
 export const routes: Routes = [
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    data: {
+      title: "Welcome!"
+    }
   },
   {
     path: 'classes',
-    component: ClassesComponent
+    component: ClassesComponent,
+    data: {
+      title: "Classes"
+    }
   },
   {
     path: '**',
