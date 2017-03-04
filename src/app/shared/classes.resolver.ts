5b73fe0bae7ce9bd2b4dd2782ced010dc29a1d9f
Raju Gandhi <raju.gandhi@gmail.com>
2017-03-04 07:13:35 -0500

    fix: remove unused dependencies and clean up imports

--- src/app/shared/classes.resolver.ts
+++ src/app/shared/classes.resolver.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class ClassesResolver implements Resolve<any> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+  resolve(): Observable<any> {
     return Observable.of({
       msg: "Data!!"
     }).delay(3000);
