9c1ca7c1d11b22d5199587aef607e19397be0a6b
Raju Gandhi <raju.gandhi@gmail.com>
2017-03-04 07:10:18 -0500

    feat: repo service use should backend to fetch classes list

--- src/app/shared/repo.service.ts
+++ src/app/shared/repo.service.ts
@@ -1,8 +1,18 @@
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { Class } from './class.interface';
+
+const BASE_URL = 'http://localhost:3200/';
 
 @Injectable()
 export class RepoService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getClasses(): Observable<Array<Class>> {
+    return this.http.get(`${BASE_URL}classes`)
+      .map(res => res.json());
+  }
 
 }
