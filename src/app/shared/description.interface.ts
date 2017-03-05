d4a11c477473a0847d8e42dda51dce21bfe569ce
Raju Gandhi <raju.gandhi@gmail.com>
2017-03-05 11:41:55 -0500

    feat: introduce a Description interface

---
+++ src/app/shared/description.interface.ts
@@ -0,0 +1,5 @@
+export interface Description {
+  id: number,
+  class_id: number,
+  description: string
+}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,4 @@
 export { RepoService } from './repo.service';
 export { ClassesResolver } from './classes.resolver';
 export { Class } from './class.interface';
+export { Description } from './description.interface';
